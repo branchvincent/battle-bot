7fd9cc7627c93e32c9cde15495785c76b5805387
Ted Zhu <tedz2usa@gmail.com>
2016-09-25 20:04:55 -0400

    Setup left and right motors.

--- brstbot/brstbot.ino
+++ brstbot/brstbot.ino
@@ -1,3 +1,16 @@
+#include <AFMotor.h>
+
+AF_DCMotor motorLeft(1, MOTOR12_64KHZ);  // Initialize the left motor.
+AF_DCMotor motorRight(2, MOTOR12_64KHZ);  // Initialize the left motor.
+
+
+// Left
+// Black -> Top
+// Red -> Bottom
+
+// Right
+// Black -> Top
+// Red -> Bottom
 
 int x = 0;
 
@@ -7,10 +20,26 @@ void setup() {
   Serial.begin(9600);
   Serial.println("Hello World!");
 
+  Serial.println("Motor test!");
+
+  motorLeft.setSpeed(200);
+  motorRight.setSpeed(200);
+  
+  motorLeft.run(FORWARD);
+  motorRight.run(FORWARD);
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+
+  delay(3000);
+  motorLeft.run(RELEASE);
+  motorRight.run(RELEASE);
+  
   x++;
   Serial.println(x);
+
 }
+
+
