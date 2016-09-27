2ebbb979c4a2813e503a85a4bab0ad91801069ec
Ted Zhu <tedz2usa@gmail.com>
2016-09-26 21:52:23 -0400

    Add link to arduino motor shield library.

--- brstbot/brstbot.ino
+++ brstbot/brstbot.ino
@@ -3,9 +3,9 @@
 
 // Requires installation of the following libraries:
 /*
+ * https://github.com/adafruit/Adafruit-Motor-Shield-library
  * https://github.com/adafruit/Adafruit_HMC5883_Unified
  * https://github.com/adafruit/Adafruit_Sensor
- * 
  * 
  */
 
