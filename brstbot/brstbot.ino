034de16edb078e36d78ebcdb1093f33dee9581fe
Ted Zhu <tedz2usa@gmail.com>
2016-09-27 21:40:24 -0400

    Allow for n-based average sampling of magnetometer.

--- brstbot/brstbot.ino
+++ brstbot/brstbot.ino
@@ -53,41 +53,28 @@ void setup() {
 
 }
 
+int n = 100;
+int count = 0;
+float sum = 0;
+
 void loop() {
   parse_serial_command();
   //delay(2000);
   //b.stopMotors();
 
-  sensors_event_t event;
-  mag.getEvent(&event);
+  sum += read_mag_heading();
+  count++;
 
-//  log("X: ", event.magnetic.x); 
-//  log("Y: ", event.magnetic.y);
-//  log("Z: ", event.magnetic.z);
+  if (count > n) {
+    log((int)(sum / count));
+    sum = 0;
+    count = 0;
+    // Report new average.
+  }
 
-  // Hold the module so that Z is pointing 'up' and you can measure the heading with x&y
-  // Calculate heading when the magnetometer is level, then correct for signs of axis.
-  float heading = atan2(event.magnetic.y, event.magnetic.x);
-  
-  // Once you have your heading, you must then add your 'Declination Angle', which is the 'Error' of the magnetic field in your location.
-  // Find yours here: http://www.magnetic-declination.com/
-  // Mine is: -13* 2' W, which is ~13 Degrees, or (which we need) 0.22 radians
-  // If you cannot find your Declination, comment out these two lines, your compass will be slightly off.
-  float declinationAngle = 0.22;
-  heading += declinationAngle;
-  
-  // Correct for when signs are reversed.
-  if(heading < 0)
-    heading += 2*PI;
-    
-  // Check for wrap due to addition of declination.
-  if(heading > 2*PI)
-    heading -= 2*PI;
-   
-  // Convert radians to degrees for readability.
-  float headingDegrees = heading * 180/M_PI; 
-  
-  Serial.print("Heading (degrees): "); Serial.println(headingDegrees); //log("");
+  //log(read_mag_heading());
+
+
 
   
   
