4218a36512075d674d0ce851230eef40231293b8
Ted Zhu <tedz2usa@gmail.com>
2016-10-01 20:22:47 -0400

    New try

---
+++ brstbotnew/brstbotnew.ino
@@ -0,0 +1,39 @@
+
+
+
+class BRSTbot {
+
+  Op * op;
+
+
+  
+}
+
+
+class Op {
+
+  
+  void act_on_bot(BRSTbot & bot) {
+    
+  }
+  
+}
+
+
+
+
+void setup() {
+
+  BRSTbot b;
+
+  Op op;
+
+  
+  
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
